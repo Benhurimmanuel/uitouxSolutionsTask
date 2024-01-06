d65eb93b2e8fc3195173817893664bdc970dd87f
benhurimmanuel <benhurimmanuel@gmail.com>
2024-01-07 01:21:29 +0530

    final working commit

--- HELPERS/SWAGGER/DOCS/basicInfo.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  openapi: '3.0.3', // present supported openapi version
-  info: {
-    title: 'MUQQABLA API', // short title.
-    description: "APi's for MUQQABLA USER AND VENDOR APPLICATIONS", //  desc.
-    version: '5.0.0', // version number
-
-  },
-};
